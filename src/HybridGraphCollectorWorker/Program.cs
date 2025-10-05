c37135a78ee9d16a19e79e0937eea8f51756a19b
DeeDee1103 <61173060+DeeDee1103@users.noreply.github.com>
2025-10-04 20:02:30 -0400

    Comitting new changes

--- src/HybridGraphCollectorWorker/Program.cs
+++ src/HybridGraphCollectorWorker/Program.cs
@@ -10,10 +10,10 @@ builder.Services.AddHostedService<Worker>();
 // Add HTTP client for API communication
 builder.Services.AddHttpClient<IEDiscoveryApiClient, EDiscoveryApiClient>();
 
-// Add application services
-builder.Services.AddScoped<IGraphCollectorService, GraphCollectorService>();
-builder.Services.AddScoped<IAutoRouterService, AutoRouterService>();
-builder.Services.AddScoped<IEDiscoveryApiClient, EDiscoveryApiClient>();
+// Add application services as Singleton to match IHostedService lifetime
+builder.Services.AddSingleton<IGraphCollectorService, GraphCollectorService>();
+builder.Services.AddSingleton<IAutoRouterService, AutoRouterService>();
+builder.Services.AddSingleton<IEDiscoveryApiClient, EDiscoveryApiClient>();
 
 var host = builder.Build();
 host.Run();
